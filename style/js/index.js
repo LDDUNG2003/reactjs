fad38dd02656af4ac90921d9730ebc5f413d80eb
LDDUNG2003 <dungldcntt58@gmail.com>
2022-09-21 09:26:28 +0700

    reactjs code new

---
+++ style/js/index.js
@@ -0,0 +1,8 @@
+const app = (
+    <div className = "warp">
+        <div className="header">Header</div>
+        <div className="container">container</div>
+        <div className="footer">footer</div>
+    </div>
+)
+ReactDOM.render(app, Document.getElementById('root'))
